3a6855fe252cbad63765891e44af456054ab4e3c
yana mulyana <yanamyn15@gmail.com>
2020-02-19 01:25:28 +0700

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by yana mulyana on 19/02/20.
+//  Copyright © 2020 LinkAJa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
